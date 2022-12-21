5305ebcdd5a3e5c73ce4a54e29593f5fbf35f125
Yuta Saito <kateinoigakukun@gmail.com>
2022-12-21 03:44:02 +0000

    test: show progress and final conclusion

--- tools/run-make-test.sh
+++ tools/run-make-test.sh
@@ -8,16 +8,35 @@ TMPDIR_BASE="$(mktemp -d)"
 
 tests=$(ls -1F "$TESTS_DIR" | grep /)
 tests_count="$(echo "$tests" | wc -l)"
+tests_count_width="$(printf $tests_count | wc -c)"
 tests_index=1
+
+fail=0
 
 while IFS= read -r test; do
   testdir="$TESTS_DIR/$test"
-  echo "[$tests_index/$tests_count] Running $test"
+  printf "[%"$tests_count_width"s/%"$tests_count_width"s] Running $test " "$tests_index" "$tests_count"
+  set +e
   (
     cd "$testdir" && \
     tmpdir="$TMPDIR_BASE/$(basename "$testdir")" && \
     mkdir -p "$tmpdir" && \
     make check --silent TMPDIR="$tmpdir"
   )
+  status_code="$?"
+  set -e
+  if [[ "$status_code" -eq 0 ]]; then
+    printf "[\e[32mOK\e[0m]\n"
+  else
+    printf "[\e[31mFAILED\e[0m]\n"
+    fail=1
+  fi
   tests_index=$((tests_index+1))
 done <<< "$tests"
+
+if [[ "$fail" -eq 0 ]]; then
+  printf "\e[32mAll tests passed!\e[0m\n"
+else
+  printf "\e[31mSome tests failed!\e[0m\n"
+  exit 1
+fi
