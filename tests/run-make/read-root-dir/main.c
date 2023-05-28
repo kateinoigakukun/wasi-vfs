3b0729b380a3c4f334ab17bb2575c07bdb3cd27f
Yuta Saito <kateinoigakukun@gmail.com>
2023-05-28 23:40:51 +0000

    Update test case to accept ENOENT for missing preopens

--- tests/run-make/read-root-dir/main.c
+++ tests/run-make/read-root-dir/main.c
@@ -5,7 +5,9 @@
 
 int main(int argc, char *argv[]) {
   int root = open("/", O_RDONLY, O_DIRECTORY);
-  assert(errno == ENOTCAPABLE);
+  // wasi-libc returns ENOENT since wasi-sdk 19, but returns ENOTCAPABLE before that
+  // https://github.com/WebAssembly/wasi-libc/pull/370
+  assert(errno == ENOENT || errno == ENOTCAPABLE);
   assert(root == -1);
   return 0;
 }
