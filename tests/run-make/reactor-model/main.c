c31406651348e949db219e5e7c8e2a8798a2a030
Yuta Saito <kateinoigakukun@gmail.com>
2023-05-29 10:34:42 +0000

    Add test case to kill missing preopen register bug

--- tests/run-make/reactor-model/main.c
+++ tests/run-make/reactor-model/main.c
@@ -4,8 +4,10 @@
 #pragma clang diagnostic ignored "-Wunknown-attributes"
 __attribute__((export_name("check")))
 void check(void) {
-  check_file_exists("/hello.txt");
+  check_file_exists("/foo.txt");
   check_dir_entry_size("/", 1);
+  check_file_exists("/run/bar.txt");
+  check_dir_entry_size("/run", 1);
 }
 
 int main(int argc, char *argv[]) {
